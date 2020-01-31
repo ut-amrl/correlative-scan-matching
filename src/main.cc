d2d4cb87b7cd5b2fd024823d3b61496ec8fbd8d4
Kavan Sikand <kvsikand@gmail.com>
2020-01-31 15:41:02 -0600

    fix bag file iteration

--- src/main.cc
+++ src/main.cc
@@ -9,6 +9,7 @@
 #include "CorrelativeScanMatcher/CorrScanMatchInputMsg.h"
 #include "Eigen/Dense"
 #include "pointcloud_helpers.h"
+#include "CorrelativeScanMatcher.h"
 
 using std::string;
 using std::vector;
@@ -17,6 +18,10 @@ using CorrelativeScanMatcher::CorrScanMatchInputMsg;
 using sensor_msgs::PointCloud2;
 using Eigen::Vector2f;
 
+/*
+This allows you to either provide a scan_match_topic, in which case it functions as a ros node listening for pairs of scans on that topic.
+Alternatively, you can provide a bag file, a lidar_topic, and a pair of timestamps, and it will do scan matching for that pair and then terminate.
+*/
 DEFINE_string(
   scan_match_topic,
   "",
@@ -60,27 +65,26 @@ void scan_match_bag_file(string bag_path, double base_timestamp, double match_ti
   vector<string> topics;
   topics.emplace_back(FLAGS_lidar_topic.c_str());
   rosbag::View view(bag, rosbag::TopicQuery(topics));
+  printf("Bag file has %d scans\n", view.size());
   // Iterate through the bag
   for (rosbag::View::iterator it = view.begin();
-       ros::ok() && it != view.end();
+       it != view.end();
        ++it) {
     const rosbag::MessageInstance &message = *it;
     {
       // Load all the point clouds into memory.
       sensor_msgs::LaserScanPtr laser_scan =
               message.instantiate<sensor_msgs::LaserScan>();
-      printf("here-1\n");
       if (laser_scan != nullptr) {
-        printf("here0\n");
+        
         // Process the laser scan
         // check if the timestamp lines up
         double scan_time = laser_scan->header.stamp.sec + laser_scan->header.stamp.nsec * 1e-9;
-
-        if (abs(scan_time - base_timestamp) <= __DBL_EPSILON__) {
-          printf("HERE1\n");
+        if (abs(scan_time - base_timestamp) <= 1e-4) {
+          printf("Found Base Scan\n");
           baseCloud = pointcloud_helpers::LaserScanToPointCloud(*laser_scan, laser_scan->range_max);
-        } else if (abs(scan_time - match_timestamp) <= __DBL_EPSILON__) {
-          printf("HERE2\n");
+        } else if (abs(scan_time - match_timestamp) <= 1e-4) {
+          printf("Found Match Scan\n");
           matchCloud = pointcloud_helpers::LaserScanToPointCloud(*laser_scan, laser_scan->range_max);
         }
       }
